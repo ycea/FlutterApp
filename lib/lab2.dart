02daf250cffffa9785862432296794c772ebf9c3
Roman <roman.savinov.050@gmail.com>
2025-09-02 20:32:52 +0400

    lab 2?

---
+++ lib/lab2.dart
@@ -0,0 +1,45 @@
+import 'dart:math';
+
+void main() async {
+  SequenceGenerator seq = new SequenceGenerator(12,-1);
+  await seq.generate(10);
+  print(seq.sum);
+  switch(seq.getSequenceType()){
+    case SequenceType.positive:
+      print("Positive sequence");
+    case SequenceType.negative:
+      print("Negative sequence");
+    default:
+      print("Unknown Sequence");
+  }
+}
+enum SequenceType{
+  positive,
+  negative
+}
+class SequenceGenerator{
+  int start;
+  int step;
+  SequenceType sequenceType;
+  List<int> sequence = [];
+  SequenceGenerator(this.start, this.step)
+    : sequenceType = step >= 0 ? SequenceType.positive : SequenceType.negative;
+  Future<void> generate(int length) async{
+    sequence.clear();
+    int lastElement = start;
+    for(int i = 0; i < length; i++){
+      await addElement(sequence, lastElement);
+      lastElement+=step;
+    }
+  }
+  Future<void> addElement<T>(List<T> collection, T element) async{
+    await Future.delayed(Duration(milliseconds: 500));
+    collection.add(element);
+  }
+  SequenceType getSequenceType(){
+    return sequenceType;
+  }
+}
+extension SequenceExtensionX on SequenceGenerator{
+  int get sum => sequence.reduce( ( a, b) =>a+b );
+}
