c354cfae96b3c46a4ac83921abec90a0589fc2fe
Roman <roman.savinov.050@gmail.com>
2025-09-28 08:17:15 +0400

    little fixes

--- lib/data/mappers/MangaDtoToModelMapper.dart
+++ lib/data/mappers/MangaDtoToModelMapper.dart
@@ -2,11 +2,16 @@ import 'package:flutter_test_app/data/dtos/mangas_dto.dart';
 import 'package:flutter_test_app/domain/models/card.dart';
 import 'package:flutter_test_app/presentation/details_page/MangaDetails.dart';
 
+const _imagePlaceholder =
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png";
+
 extension MangaDtoToModelMapper on MangaDto {
   CardData toDomain() {
     return CardData(
       title ?? "UNKNOWN",
-      imageUrl: "https://uploads.mangadex.org/covers/$id/$coverImage",
+      imageUrl: coverImage == null
+          ? _imagePlaceholder
+          : "https://uploads.mangadex.org/covers/$id/$coverImage",
       description: description ?? "UNKNOWN",
     );
   }
